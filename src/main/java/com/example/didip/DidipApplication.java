ad73a16bff47c3fa73eb1fc16d46878d103f7596
laveberry <laveberry7@gmail.com>
2023-09-16 10:06:56 +0900

    DI, DIP 프로젝트 생성

---
+++ src/main/java/com/example/didip/DidipApplication.java
@@ -0,0 +1,13 @@
+package com.example.didip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DidipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DidipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/didip/DidipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.didip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DidipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
