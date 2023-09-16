191c6e9ffc9534a6446e3be0c1eb4c30bae6484d
laveberry <laveberry7@gmail.com>
2023-09-16 16:15:49 +0900

    Testability 낮은사례 추가

--- src/main/java/com/example/didip/testability/Example.java
+++ src/main/java/com/example/didip/testability/Example.java
@@ -1,9 +1,15 @@
 package com.example.didip.testability;
 
+import java.io.File;
+
 /*
 Testability : 얼마나 쉽게 Input을 변경하고, Output을 쉽게 검증할 수 있는가 ?
  */
 public class Example {
+
+    // Testability 낮은사례 - [input - 하드코딩] - 파일이 존재하지 않을때는 테스트 할 수 없다.
+    private static final File file = new File("data.txt");
+
     // Testability 낮은사례 - [input - 외부 시스템] - 하드 코딩된 외부 시스템과 연동 되어있는 경우
     public void processDataV1(String data) {
         String processedData = data.toUpperCase(); //데이터 처리
